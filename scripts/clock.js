d12681eaa4fd950809f8b9898d8c2c39f7353f11
Anya Harter <aharter@redhat.com>
2018-06-26 16:55:25 -0400

    slightly scalable clock

--- scripts/clock.js
+++ scripts/clock.js
@@ -9,7 +9,20 @@ var Ypos=225;
 var Xpos=225;
 var Ybase=32;
 var Xbase=32;
+var Ydig=160;
+var Xdig=160;
 var dots=12;
+
+document.addEventListener('DOMContentLoaded', function () {
+  var clockheight = document.getElementById("clock").offsetHeight;
+  var clockwidth = document.getElementById("clock").offsetWidth;
+  Ypos = clockheight/2;
+  Xpos = clockwidth/2;
+  Ybase = clockheight/14;
+  Xbase = clockwidth/14;
+  Ydig = clockheight/2.8;
+  Xdig = clockwidth/2.8;
+});
 
 function clock(){
   var time=new Date ();
@@ -22,8 +35,8 @@ function clock(){
   document.getElementById("center").style.top=Ypos + "px";
   document.getElementById("center").style.left=Xpos + "px";
   for (i=0; i < dots; ++i){
-    document.getElementById("dig" + (i+1)).style.top=Ypos-15+160*Math.sin(-0.49+dots+i/1.9).toString() + "px";
-    document.getElementById("dig" + (i+1)).style.left=Xpos-14+160*Math.cos(-0.49+dots+i/1.9).toString() + "px";
+    document.getElementById("dig" + (i+1)).style.top=Ypos-25+Ydig*Math.sin(-0.49+dots+i/1.9).toString() + "px";
+    document.getElementById("dig" + (i+1)).style.left=Xpos-14+Xdig*Math.cos(-0.49+dots+i/1.9).toString() + "px";
   }
   for (i=0; i < S.length; i++){
     document.getElementById("sec" + (i+1)).style.top =Ypos+i*Ybase*Math.sin(sec).toString()/2.5 + "px";
